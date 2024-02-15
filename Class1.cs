c15c1340aa3988ac7b93f66c34b6aa09170887af
Stuy, Achim <AStuy@nti.biz>
2024-02-15 13:52:23 +0100

    Fix initialization by readding bootstrapper.

--- Class1.cs
+++ Class1.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Autodesk.AutoCAD.Runtime;
+﻿using Autodesk.AutoCAD.Runtime;
+
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Hosting;
+using Microsoft.Windows.ApplicationModel.DynamicDependency;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-[assembly:ExtensionApplication(typeof(WinUITest.Class1))]
-[assembly:CommandClass(typeof(WinUITest.Class1))]
+[assembly: ExtensionApplication(typeof(WinUITest.Class1))]
+[assembly: CommandClass(typeof(WinUITest.Class1))]
 
 namespace WinUITest
 {
@@ -19,6 +16,15 @@ namespace WinUITest
     {
         public void Initialize()
         {
+            Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.CurrentDocument.Editor.WriteMessage("Initializing WinUITestttt...\n");
+            try
+            {
+                Bootstrap.Initialize(0x00010004, "", new PackageVersion(0x0FA0043A08D30000), Bootstrap.InitializeOptions.OnPackageIdentity_NOOP);
+            }
+            catch (System.Exception exc)
+            {
+                Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.CurrentDocument.Editor.WriteMessage(exc.ToString() + "\n");
+            }
         }
 
         [CommandMethod(nameof(ShowWinUI))]
@@ -27,9 +33,17 @@ namespace WinUITest
             Application.Start(_ =>
             {
                 WindowsXamlManager.InitializeForCurrentThread();
-                var w = new BlankWindow1();
-                w.Closed += (s, e) => Application.Current.Exit();
-                w.Activate();
+                try
+                {
+                    var w = new BlankWindow1();
+                    w.Closed += (s, e) => Application.Current.Exit();
+                    w.Activate();
+                }
+                catch (Exception exc)
+                {
+                    Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.CurrentDocument.Editor.WriteMessage(exc.ToString() + "\n");
+                    Application.Current.Exit();
+                }
             });
         }
 
